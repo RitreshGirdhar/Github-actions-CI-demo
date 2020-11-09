bcf85100f0c81a966789c40d092ec4adbc1c752a
Ritresh Girdhar <ritgirdh@publicisgroupe.net>
2020-11-09 14:47:36 +0530

    Github action demo changes

---
+++ src/test/java/github/action/demo/PersonalJenkinsfileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package github.action.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalJenkinsfileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
